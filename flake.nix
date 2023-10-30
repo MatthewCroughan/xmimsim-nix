a207bf26af929a24cf9bd3c0d537af37257168fa
matthewcroughan <matt@croughan.sh>
2023-10-30 17:33:05 +0000

    refactor: use flake.parts and support multiple archs

--- flake.nix
+++ flake.nix
@@ -1,13 +1,20 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-  outputs = { nixpkgs, ... }:
-  let
-    pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
-  in
-  {
-    packages.x86_64-linux = rec {
-      default = xmimsim;
-      xmimsim = pkgs.callPackage ./default.nix {};
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      perSystem = { config, pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs { config.allowUnfree = true; inherit system; };
+        packages = rec {
+          default = xmimsim;
+          xmimsim = pkgs.callPackage ./default.nix {};
+        };
+      };
+    };
 }
